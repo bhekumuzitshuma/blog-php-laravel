23961c8da9359dc2a7d204e0947ac058586a4203
bhekumuzitshuma <tshumabhekumuzit@gmail.com>
2025-01-28 16:51:36 +0200

    Home page UI updates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -22,9 +22,9 @@
         @if($posts->count())
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 @foreach($posts as $post)
-                <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <article class="p-4 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
-                        <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                        <span class="text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded bg-teal-900 text-teal-500">
                             <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
                             </svg>
@@ -38,9 +38,9 @@
                     <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ $post->excerpt }}</p>
                     <div class="flex justify-between items-center">
                         <div class="flex items-center space-x-4">
-                            <img class="w-7 h-7 rounded-full" src="{{ $post->user->profile_image ?? 'https://via.placeholder.com/150' }}" alt="{{ $post->user->name }} avatar" />
-                            <span class="font-medium dark:text-white">
-                                {{ $post->user->name }}
+                            <img class="w-7 h-7 rounded-full" src="{{ $post->user->profile_image ?? 'https://i.pravatar.cc/300' }}" alt="{{ $post->user->name }} avatar" />
+                            <span class="font-medium text-xs dark:text-white">
+                                By {{ $post->user->name }}
                             </span>
                         </div>
                         <a href="{{ route('posts.show', $post) }}" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
